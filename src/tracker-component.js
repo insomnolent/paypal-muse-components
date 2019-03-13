753ee554a955437ed187dd1fa5be3d5c9cf883cc
Christine <chrsun@paypal.com>
2019-03-12 23:59:58 -0700

    make addToCart call setCart

--- src/tracker-component.js
+++ src/tracker-component.js
@@ -1,31 +1,21 @@
 /* @flow */
 
-import { getClientID, getMerchantID } from '@paypal/sdk-client/src';
+import { getClientID } from '@paypal/sdk-client/src';
 
 type TrackingType = 'view' | 'cartEvent' | 'purchase';
 
 type CartEventType = 'addToCart' | 'setCart' | 'removeFromCart';
 
-// $FlowFixMe
-type ParamsToBeaconUrl<T> = ({ trackingType : TrackingType, data : T }) => string;
-
-type Config = {|
-    user : {
-        id : string,
-        email? : string, // mandatory if unbranded cart recovery
-        name? : string
-    },
-    property? : {
-        id : string
-    },
-    paramsToBeaconUrl? : ParamsToBeaconUrl
+type UserData = {|
+    userID : string,
+    userName : string
 |};
 
 type Product = {|
     id : string,
-    url : string,
     sku? : string,
     name? : string,
+    url? : string,
     description? : string,
     imgUrl? : string,
     otherImages? : $ReadOnlyArray<string>
@@ -45,29 +35,23 @@ type RemoveCart = {|
     items : $ReadOnlyArray<{ id : string }>
 |};
 
-const track = <T>(config : Config, trackingType : TrackingType, trackingData : T) : Promise<void> => {
+const user = { id: undefined, name: undefined };
+
+const track = <T>(userData : UserData, trackingType : TrackingType, trackingData : T) : Promise<void> => {
     const encodeData = data => encodeURIComponent(btoa(JSON.stringify(data)));
 
     return new Promise((resolve, reject) => {
         const img = document.createElement('img');
         img.style.display = 'none';
-
-        const data = {
-            ...trackingData,
-            user:       config.user,
-            property:   config.property,
-            trackingType,
-            clientId:   getClientID(),
-            merchantId: getMerchantID()
-        };
-
-        if (config.paramsToBeaconUrl) {
-            img.src = config.paramsToBeaconUrl({ trackingType, data });
-        } else {
-            img.src = `https://api.keen.io/3.0/projects/5b903f6ec9e77c00013bc6a7/events/${ trackingType }?api_key=700B56FBE7A2A6BD845B82F9014ED6628943091AD5B0A5751C3027CFE8C5555448C6E11302BD769FCC5BDD003C3DE8282C4FC8FE279A0AAC866F2C97010468197B98779B872EFD7EE980D2986503B843DA3797C750DAA00017DC8186078EADA6&data=${ encodeData(data) }`;
-            // img.src = `https://paypal.com/targeting/track?trackingType=${ trackingType }&data=${ encodeData(data) }`;
-        }
-
+        img.src = `https://api.keen.io/3.0/projects/5b903f6ec9e77c00013bc6a7/events/${ trackingType }?api_key=700B56FBE7A2A6BD845B82F9014ED6628943091AD5B0A5751C3027CFE8C5555448C6E11302BD769FCC5BDD003C3DE8282C4FC8FE279A0AAC866F2C97010468197B98779B872EFD7EE980D2986503B843DA3797C750DAA00017DC8186078EADA6&data=${ encodeData(
+            {
+                ...userData,
+                ...trackingData,
+                userId:   user.id,
+                userName: user.name,
+                clientId: getClientID()
+            }
+        ) }`;
         img.addEventListener('load', () => resolve());
         img.addEventListener('error', reject);
         if (document.body) {
@@ -76,18 +60,23 @@ const track = <T>(config : Config, trackingType : TrackingType, trackingData : T
     });
 };
 
-const trackCartEvent = <T>(config : Config, cartEventType : CartEventType, trackingData : T) : Promise<void> =>
-    track(config, 'cartEvent', { ...trackingData, cartEventType });
+const trackCartEvent = <T>(userData : UserData, cartEventType : CartEventType, trackingData : T) : Promise<void> =>
+    track(userData, 'cartEvent', { ...trackingData, cartEventType });
 
-export const Tracker = (config : Config) => ({
-    view:           (data : { pageUrl : string }) => track(config, 'view', data),
-    addToCart:      (data : Cart) => trackCartEvent(config, 'addToCart', data),
-    setCart:        (data : Cart) => trackCartEvent(config, 'setCart', data),
-    removeFromCart: (data : RemoveCart) => trackCartEvent(config, 'removeFromCart', data),
-    purchase:       (data : { cartId : string }) => track(config, 'purchase', data),
-    setUser:        (data : { user : { id : string, name? : string, email? : string } }) => {
-        config.user.id = data.user.id;
-        config.user.name = data.user.name;
-        config.user.email = data.user.email;
-    }
+const allCarts = JSON.parse(localStorage.getItem('paypal-cr-cart') || '{}');
+
+export const Tracker = (userData : UserData) => ({
+    view:           (data : { pageUrl : string }) => track(userData, 'view', data),
+    addToCart:      (data : Cart) => {
+        const cart = (allCarts[(data.cartId || 0)] || { 'items': [] });
+        data.items = (data.items || []).concat(cart.items);
+        data.price += (cart.price || 0);
+        allCarts[(data.cartId || 0)] = data;
+
+        localStorage.setItem('paypal-cr-cart', JSON.stringify(data));
+        trackCartEvent(userData, 'setCart', data);
+    },
+    setCart:        (data : Cart) => trackCartEvent(userData, 'setCart', data),
+    removeFromCart: (data : RemoveCart) => trackCartEvent(userData, 'setCart', data),
+    purchase:       (data : { cartId : string }) => track(userData, 'purchase', data)
 });
